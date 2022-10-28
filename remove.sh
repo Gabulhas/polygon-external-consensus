4899be111b68f1d23ad74b63d7ffa04c9324dc77
Guilherme Lopes <Gabulhas@users.noreply.github.com>
2022-10-28 01:29:45 +0100

    Removed "remove.sh" file

--- remove.sh
+++
@@ -1,4 +0,0 @@
-OLD_MODULE_NAME="github.com/0xPolygon/polygon-edge"
-NEW_MODULE_NAME="github.com/Gabulhas/polygon-external-consensus"
-go mod edit -module ${NEW_MODULE_NAME}
-find . -type f -name '*.go' -exec sed -i -e 's,{OLD_MODULE_NAME},{NEW_MODULE_NAME},g' {} 
